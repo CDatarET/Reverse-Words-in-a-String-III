f9a81098033592fff303b38c3894cb47dbffc297
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-09-18 21:36:15 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        strs = s.split(' ')
+        for i in range(len(strs)):
+            strs[i] = strs[i][::-1]
+        
+        ret = " ".join(strs)
+        return(ret)
+        
